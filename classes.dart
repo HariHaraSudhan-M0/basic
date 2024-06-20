05536a218fc9f027a5fb5e7320ddf15852f6dcf1
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-06-20 12:07:37 +0530

    initialized variables in the constructor

--- classes.dart
+++ classes.dart
@@ -6,6 +6,8 @@ class Person{
   int? age;
 
   Person(String name,sex, int age ){
-    
+    this.name=name;//this.name represents the variable in the class not the constructor variable
+    this.sex=sex;
+    this.age=age;
   }
 }
