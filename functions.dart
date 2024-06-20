22b21856f39eee0b8966b941406be923eea5feab
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-06-20 16:20:56 +0530

    declared 2 parameters

--- functions.dart
+++ functions.dart
@@ -1,7 +1,7 @@
 void main(){
-  abc(String name){
-    print("hello $name");
+  abc(String name1, name2){
+    print("hello $name1 and $name2");
   }
-  var cba = abc("boopesh");
+  var cba = abc("boopesh","gokul");
   print(cba);
 }
