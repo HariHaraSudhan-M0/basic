b7cac25998aceaf06c76295119dacbd86fb07252
HariHaraSudhan-M0 <m.hariharasudhan2002@gmail.com>
2024-06-20 12:11:56 +0530

    created a method

--- classes.dart
+++ classes.dart
@@ -4,10 +4,16 @@ void main(){
 class Person{
   String? name,sex;
   int? age;
-
+//constructor
   Person(String name,sex, int age ){
     this.name=name;//this.name represents the variable in the class not the constructor variable
     this.sex=sex;
     this.age=age;
   }
+//method is a function.
+//inside a class function is called as method
+  void showdata(){
+   
+  }
+
 }
